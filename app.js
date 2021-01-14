2a209894e02282460443eb428e385a0744744004
Katy <ihatetoast@gmail.com>
2021-01-13 21:25:53 -0600

    some packages added. nothing much done. played with pkgs

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-
+const validator = require("validator")
 const notes = require('./notes.js')
 const notesmsg = notes()
 console.log(notesmsg)
+console.log(validator.isEmail("ihatetoast@gmail.com"));
