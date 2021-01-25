e6e22c9ec922b9bfcc792921c9b3098bf0f64bb4
Katy <ihatetoast@gmail.com>
2021-01-25 07:38:56 -0600

    yarg commands added

--- app.js
+++ app.js
@@ -3,5 +3,25 @@ const yargs = require('yargs')
 
 const notes = require('./notes.js')
 
-console.log(process.argv);
+yargs.command({
+    command: 'add', 
+    describe: 'add a new note',
+    handler: function(){
+        console.log("New note added.");
+    }
+});
+yargs.command({
+    command: 'remove', 
+    describe: 'remove a note',
+    handler: function(){
+        console.log("Note removed.");
+    }
+});
+yargs.command({
+    command: 'edit', 
+    describe: 'edit a note',
+    handler: function(){
+        console.log("Note edited.");
+    }
+});
 console.log(yargs.argv);
