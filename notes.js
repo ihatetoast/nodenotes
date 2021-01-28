646c5da82902f6c5a302ec66123b6b23be0b2545
Katy <ihatetoast@gmail.com>
2021-01-27 21:50:17 -0600

    add note fcn finished

--- notes.js
+++ notes.js
@@ -7,12 +7,31 @@ const getNotes = function(){
 
 const addNote = function(title, body){
     const notes = loadNotes();
-    console.log(notes);
+    const duplicateNotes = notes.filter((note)=>{
+        return note.title === title
+    })
+
+    if(duplicateNotes.length === 0){
+        notes.push({
+            title: title, 
+            body: body
+        })
+        saveNotes(notes)
+        console.log("Note added successfully");
+    }else{
+        console.log("That title has been taken");
+    }
+    
+}
+
+const saveNotes = function(notes){
+    const JSONdata = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', JSONdata)
 }
 const loadNotes = function(){
     try{
         const dataBuffer = fs.readFileSync('notes.json');
-        const dataString = dataBuffer.toString(dataBuffer)
+        const dataString = dataBuffer.toString()
         return JSON.parse(dataString)
     } catch(e){
        return []
